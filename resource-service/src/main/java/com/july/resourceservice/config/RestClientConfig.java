9ba097dd272761e9ec17954608a5ca1fa6952dfc
Yuliia_Saienko <Yuliia_Saienko@epam.com>
2025-12-18 17:29:13 +0100

    3. Service Discovery

--- resource-service/src/main/java/com/july/resourceservice/config/RestClientConfig.java
+++ resource-service/src/main/java/com/july/resourceservice/config/RestClientConfig.java
@@ -3,19 +3,22 @@ package com.july.resourceservice.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
-    public RestClient songServiceRestClient(@Value("${song.service.url}") String baseUrl, RestClient.Builder builder) {
+    public RestClient songServiceRestClient(@Value("${song.service.url:lb://song-service}") String baseUrl,
+                                            RestClient.Builder builder) {
         return builder
                 .baseUrl(baseUrl)
                 .build();
     }
 
     @Bean
+    @LoadBalanced
     public RestClient.Builder restClientBuilder() {
         return RestClient.builder();
     }
